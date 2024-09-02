292a63ee354d725d3a2de0333fc0d282b0d3c491
Gregor Simič <gregor.simic@iter.org>
2024-09-02 13:13:34 +0200

    Removing the old FieldLineTracer code and the external code wrapper.

--- l2g/comp/core/__init__.py
+++
@@ -1,9 +0,0 @@
-# Import the functions and classes exposed to Python
-
-from l2g.comp.core.ext_wrapper import PyEmbreeAccell, PyFLT
-
-# Process function
-from l2g.comp.core._core import processData, processDataOnPoints
-
-# Main FLT functions
-from l2g.comp.core._core import runFLT, getFL, getFlOnPoint, runFLTonPoints
