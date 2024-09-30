c3974f05a077184a462374969f669331801ceab2
Gregor Simič <gregor.simic@iter.org>
2024-09-30 16:15:36 +0200

    Remove unused module

--- l2g/utils/__init__.py
+++
@@ -1,4 +0,0 @@
-# Main function that reads mesh data
-# from ._meshio import readMesh
-# MEDCoupling function for transforming numpy array to field and vice versa
-# from ._meshio import fieldToNumpy, numpyArrayToField, writeFieldToAlreadyExistingMesh
